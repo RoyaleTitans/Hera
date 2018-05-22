fda8379144c2d49668237fc689368df3ca6b2745
kyledoescode <thenewopum@gmail.com>
2018-05-21 21:14:59 -0400

    Quick fix

--- src/main/java/com/royale/titans/hera/crypto/sodium/ClientCrypto.java
+++ src/main/java/com/royale/titans/hera/crypto/sodium/ClientCrypto.java
@@ -5,10 +5,25 @@ import com.royale.titans.hera.protocol.PiranhaMessage;
 public class ClientCrypto extends Crypto {
 
     public byte[] encrypt(PiranhaMessage message) {
-        return null;
+        switch (message.getId()) {
+            case 10100: {
+                return message.encode().array();
+            }
+            default: {
+                return null;
+            }
+        }
     }
 
     public byte[] decrypt(short id, byte[] data) {
-        return null;
+        switch (id) {
+            case 20100:
+            case 20103: {
+                return data;
+            }
+            default: {
+                return null;
+            }
+        }
     }
 }
